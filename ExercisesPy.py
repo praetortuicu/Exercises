3318b1672c7f513352e0f9fcc0c4003c4530a5cf
praetortuicu <39301337+praetortuicu@users.noreply.github.com>
2019-08-23 20:11:10 +0300

    Update ExercisesPy.py

--- ExercisesPy.py
+++ ExercisesPy.py
@@ -2,22 +2,28 @@ import re
 import random
 
 maxGuesses= 10                  #if ==0 lose
-words= ["dwarves", "lord", "audition", "nougat", "zebra"]
+words= ["dwarves", "lord", "audition", "nougat", "zebra", "Spock"] #list of words; editable whenever
 txt= "You now have {} more guess/es!"
 failed= int
 
 
-y= random.choice(words)         #choose some word from stash
+y= random.choice(words)         #choose some word from list
 ite= iter(y)                    #split word into iterations
+
 print("Input a letter !!CASE SENSITIVE!!")
+
 x= input()                      #user input of letter
+letter_found= y.find(x)
+def show():                     #function used to print the index of where the input is in the word
+    print(letter_found)
 
-while maxGuesses >= 0:
+while maxGuesses > 1:
     failed==0                   #variable for failure
-
-    if x in y:                 #check if input is in word
+    if x in y:                  #what to do if input exists in word
+        show()                  #show the location of input in word
         print("Good, go again!")
-        x= input()
+        x= input()              #input next letter
+
     elif x not in y:
         maxGuesses -= 1         #goes down if you get it wrong
         print("Whoops!")
